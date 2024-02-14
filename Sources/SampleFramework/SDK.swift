966ea298ecd51144a8d90e17ff408bceac1783f0
Shilpa Jagdale <shilpa@8848digital.com>
2024-02-14 20:40:30 +0530

    first commit

---
+++ Sources/SampleFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Shilpa on 14/02/24.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomeWork() {
+        print("Doing Some work....")
+    }
+}
