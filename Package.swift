957a4e02ba1153bbe15c5857730489ce914fffa7
Shilpa Jagdale <shilpa@8848digital.com>
2024-02-14 21:35:15 +0530

    version 0.0.2

--- Package.swift
+++ Package.swift
@@ -5,24 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "SampleFramework",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "SampleFramework",
-            targets: ["SampleFramework"]),
-    ],
+    platforms: [
+          .macOS(.v12), .iOS(.v15)
+        ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SampleFramework",
-            dependencies: []),
-        .testTarget(
-            name: "SampleFrameworkTests",
-            dependencies: ["SampleFramework"]),
-    ]
+           // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+           // Targets can depend on other targets in this package, and on products in packages this package depends on.
+           .binaryTarget(
+               name: "SampleFramework",
+               path: "./Sources/SampleFramework.xcframework")
+       ]
 )
